659d7f899761d67c018b476fc3ddb9fbe1d80016
Serena Hawkins <sjhawkins@ap.org>
2025-10-03 18:26:41 -0700

    Generating class and function to get artist id

--- apputil.py
+++ apputil.py
@@ -1,1 +1,11 @@
-# your code here ...
+from dotenv import load_dotenv
+load_dotenv()
+import os
+#Exercise 1 
+#Generating the class 
+class Genius: 
+    def __init__(self, access_token):
+        self.access_token = os.environ['ACCESS_TOKEN']
+
+    def get_access_token(self): 
+        return self.access_token
