6f503fc057fa6b83c36550d8c3e94a139815b4cf
Serena Hawkins <sjhawkins@ap.org>
2025-10-03 20:13:30 -0700

    Trying new function for exercise 3

--- apputil.py
+++ apputil.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 import requests
+import pandas as pd
 #Exercise 1 
 #Generating the class 
 class Genius: 
@@ -23,7 +24,7 @@ class Genius:
         artist_id = json_data["response"]["hits"][1]["result"]["id"]
         return(artist_id)
     
-    def get_artist_id(self, artist):
+    def get_artist_name(self, artist):
         """Function to get the artist name from the main response"""
         #Get the token
         token = self.get_access_token()
@@ -57,4 +58,18 @@ class Genius:
         #Use the ID to get the results
         results = self.get_artist_details(artist_id)
         return(results)
+    
+    def get_artist_data(self, search_term):
+        artist_id = self.get_artist_id(search_term)
+        artist_name = self.get_artist_name(search_term)
+        follower_count = self.get_artist(search_term)["followers_count"]
+        artist_result = [search_term, artist_name, artist_id, follower_count ]
+        return(artist_result)
+    
+    def get_artists(self, search_terms):
+        return_data = pd.DataFrame(columns=["search_term", "artist_name", "artist_id", "follower_count"])
+        for search_term in search_terms: 
+            temp_data = self.get_artist_data(search_term)
+            return_data.loc[len(return_data)] = temp_data
+        return(return_data)
     
