588d5f98fb47f324a8ba96e0e78c316b8f2baf7b
Serena Hawkins <sjhawkins@ap.org>
2025-10-03 19:40:14 -0700

    Trying with a new organization

--- apputil.py
+++ apputil.py
@@ -10,31 +10,49 @@ class Genius:
 
     def get_access_token(self): 
         return self.access_token
+    
+    def get_artist_id(self, artist):
+        """Function to get the artist ID from the main response"""
+        #Get the token
+        token = self.get_access_token(access_token="access_token")
+        #Construct the url
+        artist_url = f"http://api.genius.com/search?q={artist}&access_token={token}"
+        #Parse json
+        response = requests.get(artist_url)
+        json_data = response.json()
+        artist_id = json_data["response"]["hits"][1]["result"]["id"]
+        return(artist_id)
+    
+    def get_artist_details(self, artist_id):
+        """Function to take an artist id and return the details from genius"""
+    #Get the token and page length
+        per_page = 1
+        token = self.get_access_token(access_token="access_token")
+
+    #Construct the url
+        songs_url = f"http://api.genius.com/artists/{artist_id}&per_page={per_page}"
+
+    #Parse json
+        response = requests.get(songs_url, 
+                            headers={"Authorization": "Bearer " + token})
+        json_data = response.json()
+        results = json_data["response"]["artist"]
+        return(results)
+    
     def get_artist(self, search_term): 
         #Get the artist ID
-        artist_id = get_artist_id(search_term)
+        artist_id = get_artist_id(self, search_term)
         #Use the ID to get the results
         results = get_artist_details(artist_id)
         return(results)
     
 #Adding helper functions 
-def get_artist_id(artist):
-    """Function to get the artist ID from the main response"""
-    #Get the token
-    token = Genius.get_access_token()
-    #Construct the url
-    artist_url = f"http://api.genius.com/search?q={artist}&access_token={token}"
-    #Parse json
-    response = requests.get(artist_url)
-    json_data = response.json()
-    artist_id = json_data["response"]["hits"][1]["result"]["id"]
-    return(artist_id)
 
 def get_artist_details(artist_id):
     """Function to take an artist id and return the details from genius"""
     #Get the token and page length
     per_page = 1
-    token = Genius.get_access_token()
+    token = Genius.get_access_token(access_token="access_token")
 
     #Construct the url
     songs_url = f"http://api.genius.com/artists/{artist_id}&per_page={per_page}"
