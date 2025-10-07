b7453ef66b290ea177a42e45ff59e8ff6fa37b12
Serena Hawkins <sjhawkins@ap.org>
2025-10-06 20:30:53 -0400

    Trying again

--- apputil.py
+++ apputil.py
@@ -7,7 +7,7 @@ import pandas as pd
 #Generating the class 
 class Genius: 
     def __init__(self, access_token):
-        self.access_token = os.environ[access_token]
+        self.access_token = access_token
 
     def get_access_token(self): 
         return self.access_token
